b09bd6dbe04bc18d8f3f811c83fd9e7564e92220
Teye Shine <60197099+cisco-ramon-cis@users.noreply.github.com>
2022-05-31 23:32:59 +0000

    Delete tracker.py

--- Group1/tracker.py
+++
@@ -1,7 +0,0 @@
-'''
-information from this module will be sent to a web server every 5 mins
-regarding the location of the device either cordinates or rendered into 
-an image and sent
-'''
-def tr():
-    print("tracker")
